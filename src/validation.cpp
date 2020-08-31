04c9e70a9a76563b4d1c7596e2a4ac8684ef4389
chain123-team <66297861+chain123-team@users.noreply.github.com>
2020-08-31 13:30:30 +0900

    Update validation.cpp

--- src/validation.cpp
+++ src/validation.cpp
@@ -1057,7 +1057,7 @@ CAmount GetBlockSubsidy(int nHeight, const Consensus::Params& consensusParams)
 	}
 	
     // Subsidy is cut in half every 210,000 blocks which will occur approximately every 4 years.
-    nSubsidy >>= halvings;
+    //nSubsidy >>= halvings;
     return nSubsidy;
 }
 
